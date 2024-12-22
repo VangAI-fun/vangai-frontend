879342c3d6ef2589afa0622e17cde54d20445221
van-vanich <panahno13@gmail.com>
2024-12-23 01:59:53 +0200

    fix issue with send sol for request to backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,17 @@ import React, {useEffect, useState} from "react";
 import BottomNav from "@/component/BottomNav";
 import {useFetchAI} from "@/hook/useFetchAI";
 import {useWallet} from "@solana/wallet-adapter-react";
-import {Connection, PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+import {Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
 
 export default function Home() {
-    console.log("AAA")
     const content = "what do I need do with relationship with AI Agent?"
     const {data} = useFetchAI(content);
 
     const {publicKey, connected, sendTransaction} = useWallet();
     const [transactionStatus, setTransactionStatus] = useState<string>('');
-    const recipient = '9hUyPGeYFxdfrp5XmrkaVoEbB1V8hjuigXdUAM7LQTXi';
+    const recipient = 'D9Pn2KWJoheQ78K5tXZZ1TgQHTWYkEizdUkp4kDAQmN9';
+    const connection = new Connection('https://api.devnet.solana.com', 'confirmed');
+
 
     useEffect(() => {
         if (connected) {
@@ -24,38 +25,31 @@ export default function Home() {
         }
     }, [connected, publicKey]);
 
-    const handleTransfer = async () => {
-        if (!connected || !publicKey) {
-            setTransactionStatus('Wallet is not connected');
+
+    const handleGift = async () => {
+        if (!publicKey) {
+            alert("Please connect your wallet.");
+            return;
+        }
+        const number = 0.005;
+
+        if (isNaN(number)) {
+            alert("The input cannot be converted to a number.");
             return;
         }
 
-        if (!recipient) {
-            setTransactionStatus('Please enter a valid recipient address and amount');
-            return;
-        }
+        const transaction = new Transaction().add(
+            SystemProgram.transfer({
+                fromPubkey: publicKey,
+                toPubkey: new PublicKey(recipient),
+                lamports: number * LAMPORTS_PER_SOL,
+            })
+        );
 
-        try {
-            const connection = new Connection('https://api.devnet.solana.com');
-            const transaction = new Transaction().add(
-                SystemProgram.transfer({
-                    fromPubkey: publicKey,
-                    toPubkey: new PublicKey(recipient),
-                    lamports: 0.005 * 1e9,
-                })
-            );
+        const signature = await sendTransaction(transaction, connection);
 
-            const signature = await sendTransaction(transaction, connection);
-            setTransactionStatus(`Transaction sent! Signature: ${signature}`);
-
-            await connection.confirmTransaction(signature, 'processed');
-
-
-        } catch (error) {
-            console.error('Error during transaction:', error);
-            setTransactionStatus('Transaction failed');
-        }
-    };
+        await connection.confirmTransaction(signature, 'confirmed');
+    }
 
     return (
         <div>
@@ -74,7 +68,7 @@ export default function Home() {
                     <button
                         className="absolute left-10 top-96 bg-purple-700 ml-64 text-white pl-24 pr-24 pt-2 pb-2 z-30"
                         style={{top: 'calc(24rem + 325px)'}}
-                        onClick={handleTransfer}
+                        onClick={handleGift}
                     >
                         ASK 0.005 SOL
                     </button>
